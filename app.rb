7a2bc41926afa8b0ab266b9fb33ef0a3a3054ace
Khurram Virani <kvirani@gmail.com>
2015-02-17 10:10:36 -0800

    methodizing my pets

--- app.rb
+++ app.rb
@@ -1,26 +1,39 @@
 require 'pg'
 
-conn = PG.connect( 
-  dbname: 'feb2015',
-  host: 'localhost',
-  port: 5432
-)
-
-pets = []
-conn.exec( "SELECT * FROM pets;" ) do |records|
-  records.each do |record|
-    pets << Pet.new(record)
-  end
-end
-
-
 class Pet
 
   attr_reader :id, :name, :attack_power, :age, :breed
 
   def initialize(fields)
-    puts "in initialize"
+    @id = fields['id']
+    @name = fields['name']
+    @attack_power = fields['attack_power']
+    @age = fields['age']
+    @breed = fields['breed']
   end
 
 end
+
+def connection
+  conn = PG.connect( 
+    dbname: 'feb2015',
+    host: 'localhost',
+    port: 5432
+  )
+end
+
+def get_pets
+  pets = []
+  connection.exec( "SELECT * FROM pets;" ) do |records|
+    records.each do |record|
+      p = Pet.new(record)
+      puts p.inspect
+      pets << p
+    end
+  end
+end
+
+get_pets
+
+
 
